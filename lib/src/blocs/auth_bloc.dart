ee0ad306b9c4923edd38ca4b96754b1fbc6b259f
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-03-17 00:07:21 +0700

    Commit lại sau khi reset

---
+++ lib/src/blocs/auth_bloc.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+class AuthBloc {
+  // Khai báo StreamControllers
+  final StreamController _nameController = StreamController();
+  final StreamController _emailController = StreamController();
+  final StreamController _passController = StreamController();
+  final StreamController _phoneController = StreamController();
+
+  // Getter để truy cập Stream
+  Stream get nameStream => _nameController.stream;
+  Stream get emailStream => _emailController.stream;
+  Stream get passStream => _passController.stream;
+  Stream get phoneStream => _phoneController.stream;
+
+  // Hàm kiểm tra tính hợp lệ
+  bool isValid(String name, String email, String pass, String phone) {
+    bool isValid = true; // Ban đầu giả định tất cả hợp lệ
+
+    if (name.isEmpty) {
+      _nameController.sink.addError("Nhập tên");
+      isValid = false;
+    } else {
+      _nameController.sink.add("");
+    }
+
+    if (phone.isEmpty) {
+      _phoneController.sink.addError("Nhập số điện thoại");
+      isValid = false;
+    } else {
+      _phoneController.sink.add("");
+    }
+
+    if (email.isEmpty) {
+      _emailController.sink.addError("Nhập email hợp lệ");
+      isValid = false;
+    } else {
+      _emailController.sink.add("");
+    }
+
+    if (pass.isEmpty) {
+      _passController.sink.addError("Nhập mật khẩu");
+      isValid = false;
+    } else {
+      _passController.sink.add("");
+    }
+
+    return isValid;
+  }
+
+  void dispose() {
+    _emailController.close();
+    _nameController.close();
+    _passController.close();
+    _phoneController.close();
+  }
+}
---
+++ lib/src/config/configs.dart
@@ -0,0 +1,3 @@
+class Configs {
+  static const String ggKEY2 = "AIzaSyB6sX6V1y9owR5_DFOIbqYoCjktd1RORVs";
+}
